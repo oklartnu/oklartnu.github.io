303f707ed1a381ee5dcdfb49a3142d3d03c0ff7c
seijsing <fredrik.seijsing@gmail.com>
2016-03-02 08:22:51 +0100

    Minor fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,6 +26,7 @@ $(document).ready(function() {
       html = '<h2><i class="icon-'+weather.code+'"></i>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<ul><li>'+weather.city+', '+weather.country+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
 
       $("#weather2").html(html);
     },
